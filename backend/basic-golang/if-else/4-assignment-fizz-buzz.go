3c092efb4d556517a9133f2549ac7d8174f76fa7
mozarik <mzeinfahrozi@gmail.com>
2022-03-12 05:16:24 +0700

    feat: add if-else

--- backend/basic-golang/if-else/4-assignment-fizz-buzz.go
+++ backend/basic-golang/if-else/4-assignment-fizz-buzz.go
@@ -1,11 +1,32 @@
-//don't show to student, ask them to do this
-
 package main
 
+import "fmt"
+
+// Quiz ini dinamakan fizzbuzz
+// Tuliskan program for loop dari 1 sampai dengan 100.
+// lalu setiap perulangan program ini.
+
+// Jika angka tersebut habis dibagi 3, maka tampilkan "fizz"
+// Jika angka tersebut habis dibagi 5, maka tampilkan "buzz"
+// Jika angka tersebut habis dibagi 3 dan 5, maka tampilkan "fizzbuzz"
+
+// Tips gunakan % (modulo) untuk mengetahui apakah angka tersebut habis dibagi 3 atau 5 atau tidak.
+
 func main() {
-	//beginanswer
-	
+	for i := 1; i <= 100; i++ {
+		//beginanswer
+		fizz := "fizz"
+		buzz := "buzz"
 
-
-	//endanswer
+		if i%3 == 0 && i%5 == 0 {
+			fmt.Println(fizz + buzz)
+		} else if i%3 == 0 {
+			fmt.Println(fizz)
+		} else if i%5 == 0 {
+			fmt.Println(buzz)
+		} else {
+			fmt.Println(i)
+		}
+		//endanswer
+	}
 }
