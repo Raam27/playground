fb105be5d4dd06aaef696163bd36fdb4436795c8
Pahlevi Fikri Auliya <pahlevi.fikri.auliya@gmail.com>
2022-03-11 09:06:07 +0700

    feat: sample golang basic code

---
+++ backend/basic-golang/if-else/2-nested-if.go
@@ -0,0 +1,13 @@
+package main
+
+func main() {
+	var a int = 10
+	var b int = 20
+	var max int
+	if a > b {
+		max = a
+	} else {
+		max = b
+	}
+	print(max)
+}
---
+++ backend/basic-golang/if-else/4-assignment-fizz-buzz.go
@@ -0,0 +1,11 @@
+//don't show to student, ask them to do this
+
+package main
+
+func main() {
+	//beginanswer
+	
+
+
+	//endanswer
+}
