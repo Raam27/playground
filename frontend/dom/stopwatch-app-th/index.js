5c7b027d42e0f676913170c90f5ace923a392d61
Dito Bagus Prasetio <56624933+ditobprasetio@users.noreply.github.com>
2022-04-02 21:19:18 +0700

    add dom assignment (#42)
    * add dom assignment
    
    * add take home assignment
    
    * revise index.js
    
    * add quote generator
    
    * add quote generator
    
    * add readme.md and revise index.js
    
    * Optimised images with calibre/image-actions
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

---
+++ frontend/dom/stopwatch-app-th/index.js
@@ -0,0 +1,85 @@
+/*
+Uncomment variable dibawah ini untuk mulai mengerjakan. dilarang mengganti nama variable yang dibuat.
+*/
+
+// let stopwatch = mendapatkan value dari stopwatch
+// let startbtn = tombol untuk memulai stapwatch
+// let stopbtn = tombol untuk memberhentikan stopwatch
+// let resetbtn = tombol untuk mereset value dari stopwatch
+
+//beginanswer
+let stopwatch = document.getElementById('stopwatch')
+let startbtn = document.getElementsByClassName('btn-start')[0]
+let stopbtn = document.getElementsByClassName('btn-stop')[0]
+let resetbtn = document.getElementsByClassName('btn-reset')[0]
+
+let hour = 0
+let min = 0
+let sec = 0
+let stoptimer = true
+
+startbtn.addEventListener('click', start)
+stopbtn.addEventListener('click', stop)
+resetbtn.addEventListener('click', reset)
+//endanswer
+
+
+function start() {
+  //beginanswer
+  if (stoptimer) {
+    stoptimer = false
+    runTime()
+  }
+  //endanswer
+}
+
+function stop() {
+  //beginanswer
+  if (!stoptimer) {
+    stoptimer = true
+  }
+  //endanswer
+}
+
+function reset() {
+  //beginanswer
+  hour = 0
+  min = 0
+  sec = 0
+
+  stopwatch.innerHTML = '00:00:00'
+  //endanswer
+}
+
+function runTime() {
+  //beginanswer
+  if (!stoptimer) {
+    hour = parseInt(hour)
+    min = parseInt(min)
+    sec = parseInt(sec)
+
+    sec++
+
+    if (sec == 60) {
+      min++
+      sec = 0
+    }
+
+    if (min == 60) {
+      hour++
+      min = 0
+      sec = 0
+    }
+
+    if (sec < 10 || sec == 0) sec = `0${sec}`
+
+    if (min < 10 || min == 0) min = `0${min}`
+
+    if (hour < 10 || hour == 0) hour = `0${hour}`
+
+    stopwatch.innerHTML = `${hour}:${min}:${sec}`
+
+    setTimeout(runTime, 1000)
+  }
+  //endanswer
+}
